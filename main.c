9450ca1ee60557091429b580a51671b667b98fb5
Charbelakplogan2002 <acharbel02@gmail.com>
2025-10-08 12:15:19 +0000

    etape 4 : créer les conditions

--- main.c
+++ main.c
@@ -5,15 +5,48 @@ int main(){
     char symbol;
     int x;
     int y;
+    double division;
 
     printf("Bonjour cher utilisateur-ice.\n");
 
-    printf("Choisissez une opération : +, -, *, /, %% : \n");
-    scanf(" %c , &symbol");
+    printf("Choisissez un opérateur : +, -, *, /, %%,  : \n");
+    scanf("%c" , &symbol);
 
+    printf("Veuillez choisir un premier nombre : \n");
+    scanf("%d", &x);
+
+    printf("Veuillez choisir un second nombre : \n");
+    scanf("%d" , &y);
+
+    if(symbol == '+')
+
+    {
+        printf("%d + %d = %d\n", x, y, x + y);
+    }
+    else if(symbol == '-')
+
+    {
+        printf("%d - %d = %d\n", x, y, x - y);
+    }
+    else if(symbol == '*')
+    
+    {
+        printf("%d * %d = %d\n", x, y, x * y);
+    }
+    else if(symbol == '/')
+{
+        division = (double)x / (double)y;
+        printf("%d / %d = %.2f\n", x, y , division);
+}
+    else if(symbol == '%'){
+        printf("%d %% %d = %d\n", x, y, x % y);
+ }
+
+    else{
+        printf("Le symbole %c n'est pas reconnu\n", symbol);
+ }
 
 
 exit(0);
-
 
 }
